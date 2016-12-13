2f4765bc9ffc9b59dbdf9c9dd0cf5ef2705592b2
mhrzja <mhrzja>
2016-12-13 15:01:23 +0330

    taghir

--- gcd.c
+++ gcd.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 
-<<<<<<< HEAD
 int gcd(int q , int t){
 	if(q == 0 || t == 0)
 	return q + t;
@@ -10,20 +9,7 @@ int gcd(int q , int t){
 	}
 	else{
 		t -= q;
-		return gcd(q,t
-		);
-=======
-int bmm(int x , int y){
-	if(x * y == 0)
-	return x + y;
-	if (x > y){
-		x %= y;
-		return bmm(x,y);
-	}
-	else{
-		y %= x;
-		return bmm(x,y);
->>>>>>> a1aeec9319f95e0eeb9d55b262306cf137c325d0
+		return gcd(q,t);
 	}
 }
 
